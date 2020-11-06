9bfd0d36f978929c273938eabf9e62586658d197
Tyler Sinks <tyler.sinks@gmail.com>
2020-11-06 13:51:13 -0500

    add reset button .html, 2 constructors to scripts.js, tests to scripts.test.js

--- _tests_/scripts.test.js
+++ _tests_/scripts.test.js
@@ -8,13 +8,21 @@
 // Test: The constructor function will allow instances to hold count of user-selected pizzas.
 // Expect: new PizzaOrder(size/s, topping/s).toEqual("Object").withKeys("smallPizzaCount", "largePizzaCount")
 
-// Describe: PizzaOrderPrice()
+// Test: The constructor function will allow instances to hold count of user-selected toppings for a large pie.
+// Expect: new PizzaOrder("large", "toppingsCount").toEqual("Object").withKey("largeToppingsCount")
 
-// Test: It will return a number that equals the price for an instance of new PizzaOrder.
-// Expect: PizzaOrderPrice(pizzaOrder).toEqual(number)
+// Test: The constructor function will allow instances to hold count of user-selected toppings for a small pie.
+// Expect: new PizzaOrder("small", "toppingsCount").toEqual("Object").withKey("smallToppingsCount")
+
+// Describe: PizzaOrderPrice()
 
 // Test: It will store a key-value pair for large pizza price.
 // Expect: PizzaOrderPrice(this.largePrice).toEqual(number)
 
 // Test: It will store a key-value pair for small pizza price.
 // Expect: PizzaOrderPrice(this.smallPrice).toEqual(number)
+
+// Describe: calculateTotal(pizzaOrderPrice)
+
+// Test: It will return a number that equals the price for an instance of new PizzaOrderPrice.
+// Expect: calculateTotal(pizzaOrderPrice).toEqual(number)
---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// Business Logic ----- >
+
+function Pizza() {
+  this.size = "large";
+  this.toppings = [];
+}
+function PizzaOrder(pizza) {
+  this.smallPizzaCount = 0;
+  this.largePizzaCount = 0;
+  this.smallToppingsCount = 0;
+  this.largeToppingsCount = 0;
+}
