10b806772a7244899ebf66d9d2903936654b0825
Tyler Sinks <tyler.sinks@gmail.com>
2020-11-06 14:19:32 -0500

    add pseudocode user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,20 @@ function calculateTotal(pizzaOrderPrice, pizzaOrder) {
   orderTotal *= 1.08875; // add NYC sales tax
   return orderTotal;
 }
+
+// User Interface Logic ------ >
+
+$("document").ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    let pizza1 = new Pizza;
+    // reassign pizza1.size to small when small radio button checked
+    let pizza1.size = $("input:radio[name=sizePick]:checked").val();
+    if ($(""))
+    // push toppings checked for pizza1 into pizza1.toppings array
+
+    // append pizza1 details to results div
+
+    // 
+  })
+})
